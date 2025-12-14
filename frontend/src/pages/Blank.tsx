63c8cec33249a2d671afe9e330f958f90d61256a
Vu Dang Khoa <khoabuit0910@gmail.com>
2025-12-14 15:17:11 +0700

    feat(pages): update blank page template

--- frontend/src/pages/Blank.tsx
+++ frontend/src/pages/Blank.tsx
@@ -5,8 +5,8 @@ export default function Blank() {
   return (
     <div>
       <PageMeta
-        title="React.js Blank Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Blank Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="Trang trống | SmartFactory CONNECT"
+        description="Trang mẫu trống cho SmartFactory CONNECT"
       />
       <PageBreadcrumb pageTitle="Blank Page" />
       <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-gray-900 xl:px-10 xl:py-12">
