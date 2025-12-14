1950c4e3dcdd86b8bef57dbd7d0c3e16f0126c09
Vu Dang Khoa <khoabuit0910@gmail.com>
2025-12-14 15:17:10 +0700

    feat(pages): implement i18n in dashboard home page

--- frontend/src/pages/Dashboard/Home.tsx
+++ frontend/src/pages/Dashboard/Home.tsx
@@ -10,10 +10,10 @@ export default function Home() {
   return (
     <>
       <PageMeta
-        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
-        description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="Trang chủ | SmartFactory CONNECT"
+        description="Trang tổng quan quản lý nhà máy thông minh SmartFactory CONNECT"
       />
-      <div className="grid grid-cols-12 gap-4 md:gap-6">
+      <div className="p-4 grid grid-cols-12 gap-4">
         <div className="col-span-12 space-y-6 xl:col-span-7">
           <EcommerceMetrics />
 
